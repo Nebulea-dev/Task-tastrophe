795a52864a4ccc4753463a7186f77e9110adecf0
elias-bina <elias.bina58@gmail.com>
2025-01-04 22:57:09 +0100

    feat: Created portable props

---
+++ objects/props/portable_props/bomb.gd
@@ -0,0 +1,10 @@
+extends PortableProps
+
+
+func _ready() -> void:
+	readyHook()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
