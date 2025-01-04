640744063d36e7f790d55bf0d2b95863d268ef75
elias-bina <elias.bina58@gmail.com>
2025-01-04 14:26:22 +0100

    fix: Player can move even if collides on wall

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -11,6 +11,7 @@ var playerIndex : int = 0
 var targetDir : Vector2 = Vector2(0, 0)
 var curr_look : float = 0 
 var kick_distance : float = 50
+var kick_speed : float = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,7 +23,6 @@ func _process(delta: float) -> void:
 	velocity = targetDir * speed
 	if(targetDir.length() > 0.01):
 		curr_look = targetDir.angle()
-		print(curr_look)
 
 	#currentSpeed += (targetDir * speed) / acceleration_frames
 	#if currentSpeed.length() > speed:
@@ -30,10 +30,11 @@ func _process(delta: float) -> void:
 	var collided_entity: KinematicCollision2D = move_and_collide(velocity * delta);
 	if(collided_entity != null):
 		var collider : Node2D = collided_entity.get_collider()
-		
 		if (collider is CharacterBody2D):
 			collider.move_and_collide(velocity.length() * move_factor * delta * (collider.global_position - global_position).normalized())
-	
+		else:
+			move_and_collide(velocity.project(collided_entity.get_normal().orthogonal()) * delta);
+
 func _on_player_move(move_x : Array[float], move_y : Array[float]) -> void:
 	targetDir.x = move_x[playerIndex]
 	if targetDir.length() > 1:
@@ -64,4 +65,5 @@ func _on_player_kick(kick_array : Array[bool]) -> void:
 		var kick: Kick = kick_res.instantiate()
 		kick.set_global_position(position + Vector2.from_angle(curr_look) * kick_distance)
 		kick.set_global_rotation(curr_look + PI)
+		kick.set_velocity(Vector2.from_angle(curr_look) * kick_speed + velocity)
 		get_parent().add_child(kick)
--- objects/projectiles/kick.gd
+++ objects/projectiles/kick.gd
@@ -2,11 +2,20 @@ extends Node2D
 
 class_name Kick
 
+var curr_velocity = Vector2(0,0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	global_position += curr_velocity * delta
+	global_rotation = curr_velocity.angle() + PI
+
+func set_velocity(target_velocity: Vector2):
+	curr_velocity = target_velocity
+
+func _on_kick_duration_timer_timeout() -> void:
+	queue_free()
