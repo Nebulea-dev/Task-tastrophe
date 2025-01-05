977c91179313bf5f9c975aad65b1bb19a7065073
elias-bina <elias.bina58@gmail.com>
2025-01-05 17:24:59 +0100

    feat: Add number to buzzer

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -5,9 +5,9 @@ extends LevelManager
 func _ready() -> void:
 	idLevel = 3
 	isDone = false
-	var mission1: Mission = Mission.new(TypeMission.TARGET, 0, 0, false, "Move to the next box")
+	var mission1: Mission = Mission.new(TypeMission.TARGET, 0, 0, false, "Move to the target box")
 	var mission2: Mission = Mission.new(TypeMission.ZONE, 0, 1, false, "Touch Grass")
-	var mission3: Mission = Mission.new(TypeMission.WAIT_FOR_PRESS, 0, 1, false, "Press the button twice")
+	var mission3: Mission = Mission.new(TypeMission.WAIT_FOR_PRESS, 0, 1, false, "Press the button 0 twice")
 	var mission4: Mission = Mission.new(TypeMission.BOMB, 0, 1, false, "Defuse bomb 0")
 	var mission5: Mission = Mission.new(TypeMission.BOMB, 1, 1, false, "Defuse bomb 1")
 	ListMission = [mission1, mission2, mission3, mission4, mission5]
