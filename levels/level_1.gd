8629bbae075cdc107b50c6a34b3293aa370e209c
Chloé Durand <chloe200.durand@gmail.com>
2025-01-05 17:47:42 +0100

    shrine chance mission and authorisation

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 	var mission3: Mission = Mission.new(TypeMission.WAIT_FOR_PRESS, 0, 1, false, "Press the button 0 twice")
 	var mission4: Mission = Mission.new(TypeMission.BOMB, 0, 1, false, "Defuse bomb 0")
 	var mission5: Mission = Mission.new(TypeMission.BOMB, 1, 1, false, "Defuse bomb 1")
+	var mission6: Mission = Mission.new(TypeMission.SHRINECHANCE, 0, 0, false, "Succeed at the luck chance")
 	ListMission = [mission1, mission2, mission3, mission4, mission5]
 	ListPropsOther  =[]
 	readyHook()
--- objects/props/ShrineChance/shrine_of_chance.gd
+++ objects/props/ShrineChance/shrine_of_chance.gd
@@ -11,18 +11,24 @@ var playerActiveId: int = -1
 var probaSuccess :int =25
 var indexPosition=1
 var nbIndexPosition=2
-var ListPosition : Array = [Vector2 (1400,400),Vector2(400,400)]
+#var ListPosition : Array = [Vector2 (1400,400),Vector2(400,400)]
 var ListplayerEntered =[false,false,false]
-
+var playerAuthorize=0
+var idShrine=0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(ListPosition[0])
-	global_position = ListPosition[0]
-	
+	#print(ListPosition[0])
+	#global_position = ListPosition[0]
+	pass
 
+func updateText(id :int):
+	idShrine=id
+	var label :Label =get_node("Label")
+	label.text="%d" %  idShrine
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	moveDoorAnimation.play("Idle")
 	pass
 	
 	
@@ -44,12 +50,11 @@ func _on_action_pressed(playerIndex: int,player: Player ) -> void:
 	if(!isDone):
 		var rand : int = randi_range(0,99)
 		if( rand >= probaSuccess ) :
-			moveDoorAnimation.play("TryFail")
-			global_position = ListPosition[indexPosition]
-			indexPosition = (indexPosition +1 )%( nbIndexPosition -1)
+			moveDoorAnimation.play("Idle")
+			#global_position = ListPosition[indexPosition]
+			#indexPosition = (indexPosition +1 )%( nbIndexPosition -1)
 		else :
-			 
-			moveDoorAnimation.play("TrySucceed")
+			moveDoorAnimation.play("Destroy")
 			
 			var allPlayer = player.get_parent().get_children()
 			for p in allPlayer :
