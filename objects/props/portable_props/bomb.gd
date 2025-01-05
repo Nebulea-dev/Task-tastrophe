d83e7e5d88e8f5e27ffd9f75bf51e8f22effe453
elias-bina <elias.bina58@gmail.com>
2025-01-05 10:35:52 +0100

    feat: Added defusing the bomb

--- objects/props/portable_props/bomb.gd
+++ objects/props/portable_props/bomb.gd
@@ -2,6 +2,8 @@ extends PortableProps
 
 class_name Bomb
 
+signal bomb_defused
+
 func _ready() -> void:
 	readyHook()
 
@@ -9,3 +11,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	if body is Defuser:
+		bomb_defused.emit()
+		$BombDefuseSound.play()
+		self.visible = false
--- signals.gd
+++ signals.gd
@@ -7,3 +7,5 @@ signal expand_player_mission_tab(player : int)
 signal set_timer_value(time: float)
 signal start_timer
 signal stop_timer
+
+signal bomb_defused
